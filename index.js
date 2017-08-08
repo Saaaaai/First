04831608dc7cb8e2874ecb583f959bc5a28b5594
Алексей Семёнов <domino-alex@list.ru>
2017-08-08 15:46:21 +0300

    add logik
    add base files

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+ajaxRequest('succsess');
+
+//AJAX-запрос
+function ajaxRequest(getResponce){
+  let ajax = new XMLHttpRequest();
+  ajax.open('GET', './' + getResponce + '.json', false);
+  ajax.send();
+  if (ajax.status != 200){
+    alert(ajax.status + ': ' + ajax.statusText);
+  }
+  else{
+    alert(ajax.responseText);
+  }
+}
