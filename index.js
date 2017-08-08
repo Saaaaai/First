c9758ab72e66d9ec15784e2d387bedddd7e920cc
Алексей Семёнов <domino-alex@list.ru>
2017-08-08 18:29:46 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function MyForm(){
   function submit(){
     //Включаем ожидание иного статуса
     ajaxRequest('progress');
-    setResultContainer('resultContainer',jsonData.status);
+    setResultContainer('resultContainer',jsonData.status,jsonData.status);
     alert('1');
     const timeout = jsonData.timeout;
 
